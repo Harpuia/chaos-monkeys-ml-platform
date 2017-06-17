2157b9694a20e417fa9469d8c9a38c021b40117b
Yuchao Zhou <yuchao.y.zhou@gmail.com>
2017-06-17 19:44:20 -0400

    Move views folder back to parent

--- CoordinationService/app.js
+++ CoordinationService/app.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/public', express.static(__dirname + '/public'));
+app.use('/views', express.static(__dirname + '/views'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
