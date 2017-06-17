6053c4770bf0733f51a1475c556e503845abfcd3
Yuchao Zhou <yuchao.y.zhou@gmail.com>
2017-06-17 19:13:37 -0400

    Integrate views with node.js server by moving views to public folder.

--- CoordinationService/app.js
+++ CoordinationService/app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/public', express.static(__dirname + '/public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
