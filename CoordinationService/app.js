d91875be2e1fd37c7e7964b3a8e627b0180445bb
Yuchao Zhou <yuchao.y.zhou@gmail.com>
2017-06-19 14:30:05 -0400

    Add main views

--- CoordinationService/app.js
+++ CoordinationService/app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/views', express.static(__dirname + '/views'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
