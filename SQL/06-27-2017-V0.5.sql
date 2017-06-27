c73671ec0b1c621df6a8acc896a3a0a251c79f86
Yuchao Zhou <yuchao.y.zhou@gmail.com>
2017-06-27 18:44:19 -0400

    Add SQL script V0.5

---
+++ SQL/06-27-2017-V0.5.sql
@@ -0,0 +1,144 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for osx10.12 (x86_64)
+--
+-- Host: localhost    Database: ConfigurationDatabase
+-- ------------------------------------------------------
+-- Server version	5.7.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `connected_services`
+--
+
+DROP TABLE IF EXISTS `connected_services`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `connected_services` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `last_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip_address_UNIQUE` (`ip_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `connected_services`
+--
+
+LOCK TABLES `connected_services` WRITE;
+/*!40000 ALTER TABLE `connected_services` DISABLE KEYS */;
+/*!40000 ALTER TABLE `connected_services` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `datasets`
+--
+
+DROP TABLE IF EXISTS `datasets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `datasets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) NOT NULL,
+  `project_id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `format` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `datasets`
+--
+
+LOCK TABLES `datasets` WRITE;
+/*!40000 ALTER TABLE `datasets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `datasets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `experiments`
+--
+
+DROP TABLE IF EXISTS `experiments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `experiments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` varchar(255) NOT NULL,
+  `task_id` varchar(255) NOT NULL,
+  `experiment_name` varchar(255) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `last_status` varchar(25) NOT NULL,
+  `last_updated` datetime NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `experiments`
+--
+
+LOCK TABLES `experiments` WRITE;
+/*!40000 ALTER TABLE `experiments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `experiments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `formats`
+--
+
+DROP TABLE IF EXISTS `formats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `formats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `format` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `formats`
+--
+
+LOCK TABLES `formats` WRITE;
+/*!40000 ALTER TABLE `formats` DISABLE KEYS */;
+INSERT INTO `formats` VALUES (1,'JSON'),(2,'CSV');
+/*!40000 ALTER TABLE `formats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `datasets` WRITE;
+/*!40000 ALTER TABLE `datasets` DISABLE KEYS */;
+INSERT INTO `datasets` VALUES (0, 'Ingo Weber', 'Project Amazon', '100-Sleep-Data', 'amazon', 'This data set includes 100 students\' sleeping data', 'JSON'), (1, 'Len Bass', 'Project Amazon', '30-Sqrill-Data', 'amazon', 'This data set includes 100 students\' sleeping data', 'CSV');
+/*!40000 ALTER TABLE `datasets` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-06-26 19:37:29
