8490507551185a4d97ca93e229631c8feea8146c
Rachel <xinh1@andrew.cmu.edu>
2017-07-06 19:29:56 -0400

    added the 'model_id' column for 'tasks' table.

---
+++ SQL/07-06-2017-V0.8.sql
@@ -0,0 +1,415 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for osx10.12 (x86_64)
+--
+-- Host: localhost    Database: ConfigurationDatabase
+-- ------------------------------------------------------
+-- Server version	5.7.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `ConfigurationDatabase`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ConfigurationDatabase` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `ConfigurationDatabase`;
+
+--
+-- Table structure for table `algorithm_languages`
+--
+
+DROP TABLE IF EXISTS `algorithm_languages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `algorithm_languages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `language` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `algorithm_languages`
+--
+
+LOCK TABLES `algorithm_languages` WRITE;
+/*!40000 ALTER TABLE `algorithm_languages` DISABLE KEYS */;
+INSERT INTO `algorithm_languages` VALUES (0,'R'),(1,'Python');
+/*!40000 ALTER TABLE `algorithm_languages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `algorithms`
+--
+
+DROP TABLE IF EXISTS `algorithms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `algorithms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `path` varchar(300) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `language` varchar(20) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `algorithms`
+--
+
+LOCK TABLES `algorithms` WRITE;
+/*!40000 ALTER TABLE `algorithms` DISABLE KEYS */;
+INSERT INTO `algorithms` VALUES (0,'Logistic Regression','logistic-regression-ingo','This data set includes 100 msit-se students\' sleeping data......','R','ingo.weber'),(1,'Decision Tree','decision-tree-len','This data set includes 100 msit-se students\' sleeping data......','Python','len.bass');
+/*!40000 ALTER TABLE `algorithms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `connected_services`
+--
+
+DROP TABLE IF EXISTS `connected_services`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `connected_services` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `last_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip_address_UNIQUE` (`ip_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `connected_services`
+--
+
+LOCK TABLES `connected_services` WRITE;
+/*!40000 ALTER TABLE `connected_services` DISABLE KEYS */;
+/*!40000 ALTER TABLE `connected_services` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `datasets`
+--
+
+DROP TABLE IF EXISTS `datasets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `datasets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) NOT NULL,
+  `project_id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `format` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `datasets`
+--
+
+LOCK TABLES `datasets` WRITE;
+/*!40000 ALTER TABLE `datasets` DISABLE KEYS */;
+INSERT INTO `datasets` VALUES (0,'Ingo Weber','Project Amazon','100-Sleep-Data','amazon','This data set includes 100 students\' sleeping data','JSON'),(1,'Len Bass','Project Amazon','30-Sqrill-Data','amazon','This data set includes 100 students\' sleeping data','CSV');
+/*!40000 ALTER TABLE `datasets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `experiments`
+--
+
+DROP TABLE IF EXISTS `experiments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `experiments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `experiment_name` varchar(255) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `last_status` varchar(25) NOT NULL,
+  `last_updated` datetime NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `experiments`
+--
+
+LOCK TABLES `experiments` WRITE;
+/*!40000 ALTER TABLE `experiments` DISABLE KEYS */;
+INSERT INTO `experiments` VALUES (0,0,0,'Experiment 1','2017-07-01 09:00:00','2017-07-01 11:00:00','running','2017-07-01 11:05:00','sleep-analysis'),(1,1,1,'Experiment 2','2017-07-02 09:00:00','2017-07-02 11:00:00','not responding','2017-07-02 11:05:00','feature-classification');
+/*!40000 ALTER TABLE `experiments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `formats`
+--
+
+DROP TABLE IF EXISTS `formats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `formats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `format` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `formats`
+--
+
+LOCK TABLES `formats` WRITE;
+/*!40000 ALTER TABLE `formats` DISABLE KEYS */;
+INSERT INTO `formats` VALUES (1,'JSON'),(2,'CSV');
+/*!40000 ALTER TABLE `formats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `models`
+--
+
+DROP TABLE IF EXISTS `models`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `models` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `path` varchar(255) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `experiment_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `models`
+--
+
+LOCK TABLES `models` WRITE;
+/*!40000 ALTER TABLE `models` DISABLE KEYS */;
+INSERT INTO `models` VALUES (1,'model1','IDK type of format file describing the trained model','model1 path',1,1),(2,'model2','IDK type of format file describing the trained model','model2 path',2,2);
+/*!40000 ALTER TABLE `models` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `predictions`
+--
+
+DROP TABLE IF EXISTS `predictions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `predictions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `project_id` int(11) NOT NULL,
+  `experiment_id` int(11) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `predictions`
+--
+
+LOCK TABLES `predictions` WRITE;
+/*!40000 ALTER TABLE `predictions` DISABLE KEYS */;
+INSERT INTO `predictions` VALUES (1,'prediction1','XML/YAML/ARFF file with instance-level predictions generated by the model',1,1,'prediction1 path'),(2,'prediction2','XML/YAML/ARFF file with instance-level predictions generated by the model',2,2,'prediction2 path');
+/*!40000 ALTER TABLE `predictions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `private` tinyint(1) NOT NULL,
+  `owner_id` varchar(50) NOT NULL,
+  `members` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (0,'Project Amazon','Project Amazon Description',1,'ingo.weber','student'),(1,'Microsoft Friendship','Microsoft Friendship Description',1,'ingo.weber','student');
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (0,'ingo.weber','Researcher'),(1,'len.bass','Researcher');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service_types`
+--
+
+DROP TABLE IF EXISTS `service_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `service_types` (
+  `ip` varchar(100) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  PRIMARY KEY (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service_types`
+--
+
+LOCK TABLES `service_types` WRITE;
+/*!40000 ALTER TABLE `service_types` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_types`
+--
+
+DROP TABLE IF EXISTS `task_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_types`
+--
+
+LOCK TABLES `task_types` WRITE;
+/*!40000 ALTER TABLE `task_types` DISABLE KEYS */;
+INSERT INTO `task_types` VALUES (1,'Training'),(2,'Execution');
+/*!40000 ALTER TABLE `task_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `dataset_id` int(11) NOT NULL,
+  `algorithm_id` int(11) DEFAULT NULL,
+  `model_id` int(11) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `type` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (1,1,1,1,1,'Task1','Sample algorithm 1','Training'),(2,2,2,2,2,'Task2','Sample algorithm 2','Execution'),(3,3,3,3,3,'Task3','Sample algorithm 3','Training'),(4,4,4,4,4,'Task4','Sample algorithm 4','Execution'),(5,5,5,5,5,'Task5','Sample algorithm 5','Training');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `jobfunction` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (0,'ingo.weber','123456','Ingo','Weber','Researcher','Ingo.Weber@data61.csiro.au'),(1,'len.bass','123456','Len','Bass','Researcher','lenbass@cmu.edu');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-06 19:26:32
