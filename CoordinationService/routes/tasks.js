c8f567ecb143d453f9d03c18b7de24b405b7562c
Yuchao Zhou <yuchao.y.zhou@gmail.com>
2017-07-12 20:09:28 -0400

    Create algorithm detail popup

--- CoordinationService/routes/tasks.js
+++ CoordinationService/routes/tasks.js
@@ -197,4 +197,23 @@ router.get('/listByModel/:model_id', function listByModel(req, res) {
   });
 });
 
+/* List the tasks from algorithm page */
+router.get('/listByAlgorithm/:algorithm_id', function listByAlgorithm(req, res) {
+  var algorithm_id = req.params.algorithm_id;
+
+  //Connect to DB
+  var connection = createDbConnection();
+  connection.connect();
+
+  //Return all tasks
+  var results = connection.query('select name, description from tasks where algorithm_id = ' + algorithm_id, function listTask(err, rows, fields) {
+    if (err) {  // pass the err to error handler
+      err.source = 'mysql'; // add error source for tracing
+      err.status = 500;
+      next(err);
+    }
+    res.json({ tasks: rows });
+  });
+});
+
 module.exports = router;
